2f769b746336a6b819b90514a312e273a1ee1b49
Daniel Hallada <hallada.daniel@gmail.com>
2019-06-24 11:10:59 -0400

    Done.

--- src/components/restaurants/RestaurantInput.js
+++ src/components/restaurants/RestaurantInput.js
@@ -1,13 +1,31 @@
 import React, { Component } from 'react';
 
 class RestaurantInput extends Component {
+  constructor() {
+    super() 
+    this.state = {
+      text:""
+    }
+  }
+
+  handleSubmit = (event) => {
+    event.preventDefault()
+    this.props.addRestaurant(this.state.text)
+    this.setState({text:""})
+  }
+
   render() {
     return (
       <div>
-        Restaurant Input
+        <form onSubmit={(event) => this.handleSubmit(event)}>
+          <input type="text" value={this.state.text} onChange={(event) => this.setState({text:event.target.value})}/>
+          <input type="submit" />
+        </form>
       </div>
     );
   }
 };
 
-export default RestaurantInput;
+
+
+export default RestaurantInput
--- src/reducers/manageRestaurant.js
+++ src/reducers/manageRestaurant.js
@@ -1,7 +1,41 @@
 
 import cuid from 'cuid';
+import {combineReducers} from 'redux'
 export const cuidFn = cuid;
 
-export default function manageRestaurants(state, action) {
-
+function manageRestaurants(restaurants = [], action) {
+    switch (action.type) {
+        case "ADD_RESTAURANT":
+            let newRestaurant = {
+                text: action.name,
+                id: cuid()
+            }
+            return [...restaurants, newRestaurant];
+        case "DELETE_RESTAURANT":
+                return [...restaurants.filter(restaurant => restaurant.id !== action.id)];
+        default:
+            return restaurants
+    }
 }
+
+function manageReviews(reviews = [], action) {
+    switch (action.type) {
+        case "ADD_REVIEW":
+            let newReview = action.review
+            newReview.id = cuid()
+            return [...reviews, newReview]
+        case "DELETE_REVIEW":
+            return [...reviews.filter(review => review.id !== action.id)]
+        default:
+            return reviews;
+    }
+}
+
+const rootReducer = combineReducers({
+    restaurants: manageRestaurants,
+    reviews: manageReviews
+})
+
+export default rootReducer
+
+
